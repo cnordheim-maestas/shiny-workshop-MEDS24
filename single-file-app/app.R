b23ce5286c4324c24034fbf43f7c21c4827ab557
cnordheim <caitlinnordheim@ucsb.edu>
2024-01-26 10:40:58 -0800

    part 1 of workshop

---
+++ single-file-app/app.R
@@ -0,0 +1,11 @@
+# load packages ----
+library(shiny)
+
+# user interface ----
+ui <- fluidPage()
+
+# server ----
+server <- function(inputs, outputs) {}
+
+# combine ui and server into app ----
+shinyApp(ui = ui, server = server)
