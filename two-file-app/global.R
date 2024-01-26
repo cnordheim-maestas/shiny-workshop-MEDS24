66098866af8104ea3151fdff4fd10893bf6fe17a
cnordheim <caitlinnordheim@ucsb.edu>
2024-01-26 14:14:05 -0800

    building backbone of second app

--- two-file-app/global.R
+++ two-file-app/global.R
@@ -1,2 +1,26 @@
 # load packages ----
 library(shiny)
+library(tidyverse)
+library(palmerpenguins)
+library(lterdatasampler)
+library(shinyWidgets)
+
+# we want this in our global bc it makes the server cleaner and it will run before everything else when we render our app
+#.......................wrangle trout data.......................
+clean_trout <- and_vertebrates |>
+  filter(species == "Cutthroat trout") |>
+  select(sampledate, section, species, length_mm = length_1_mm, weight_g, channel_type = unittype) |> 
+  mutate(channel_type = case_when(
+    channel_type == "C" ~ "cascade",
+    channel_type == "I" ~ "riffle",
+    channel_type =="IP" ~ "isolated pool",
+    channel_type =="P" ~ "pool",
+    channel_type =="R" ~ "rapid",
+    channel_type =="S" ~ "step (small falls)",
+    channel_type =="SC" ~ "side channel"
+  )) |> 
+  mutate(section = case_when(
+    section == "CC" ~ "clear cut forest",
+    section == "OG" ~ "old growth forest"
+  )) |> 
+  drop_na()
--- two-file-app/ui.R
+++ two-file-app/ui.R
@@ -1,2 +1,79 @@
 # user interface ----
-ui <- fluidPage()
+ui <- navbarPage(
+  
+  title = "LTER Animal Data Explorer",
+  
+  # (Page 1) intro tabPanel ----
+  tabPanel(title = "About this App",
+           
+           "background info will go here" # REPLACE THIS WITH CONTENT
+           
+  ), # END (Page 1) intro tabPanel
+  
+  # (Page 2) data viz tabPanel ----
+  tabPanel(title =  "Explore the Data",
+           
+           # tabset panel to contain tabs for data vis ----
+           tabsetPanel(
+             
+             #trout tabpanel
+             tabPanel(title="Trout",
+                      
+                      # trout sidebar layout ----
+                      sidebarLayout(
+                        
+                        # trout sidebar panel ----
+                        sidebarPanel(
+                          
+                         # channel type pickerinput ----
+                         pickerInput(inputId = "channel_type_input",
+                                     label = "Select Channel Type(s):",
+                                     choices = unique(clean_trout$channel_type), # prints all options in the column channel type
+                                     selected = c("cascade", "pool"), # to be selected by default
+                                     options = pickerOptions(actionsBox = TRUE), # adds a selct all and deselect all
+                                     multiple = TRUE # can select multiple channel types onto the plot together
+                                     ) # END channel type pickerinput
+                          
+                        ), # END trout sidebar panel
+                        
+                        # trout main panel ----
+                        mainPanel(
+                          
+                          "trout output will live here"
+                          
+                        ) # END trout main panel
+                        
+                      ) #END trout sidebar layout
+                      
+             ), # END trout panel
+             
+             # penguin tabpanel
+             tabPanel(title="Penguins",
+                      
+                      # penguin sidebar layout ----
+                      sidebarLayout(
+                        
+                        # penguin sidebar panel ----  
+                        sidebarPanel(
+                          
+                          "penguin inputs go here"
+                          
+                        ), # END penguin sidebar panel
+                        
+                        # penguin main panel
+                        mainPanel(
+                          
+                          "penguin plot goes here"
+                          
+                        ) # END main panel
+                        
+                        
+                      ) # END penguin sidebar layout
+                      
+             ), # END penguin panel
+             
+           ) # end tabsetPanel
+           
+  ) # END (Page 2) data viz tabPanel
+  
+) # END navbarPage
