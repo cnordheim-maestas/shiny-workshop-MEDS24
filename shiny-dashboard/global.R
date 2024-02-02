a52f0eb24fbdcee5e66be483647775cef183a152
cnordheim <caitlinnordheim@ucsb.edu>
2024-02-02 11:50:40 -0800

    adding sliders I think

--- shiny-dashboard/global.R
+++ shiny-dashboard/global.R
@@ -1,3 +1,17 @@
 # LOAD LIBRARIES ----
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(leaflet)
+library(shinycssloaders)
+
+###### avoid using "here" inside of your apps!!!!!
+
+# The file path specified for reading in our data is relative to the app files (ui.R, server.R). 
+# It’s important to note, however, that you won’t be able to run your code line-by-line (like in a typical script; the file path won’t be recognized) – this is expected!
+# "relative to your global.r" for this app
+
+# read in data
+lake_data <- read_csv("data/lake_data_processed.csv")
+
+
--- shiny-dashboard/ui.R
+++ shiny-dashboard/ui.R
@@ -28,14 +28,84 @@ body <- dashboardBody(
     # welcome tabItem ----
     tabItem(tabName = "welcome", # identifier specified above
             
-            "background info here"
+            # left-hand column ----
+            column(width = 6,
+                   
+                   # background info box ----
+                   box(width = NULL,
+                       
+                       "background info here"
+                       
+                   ), # END background info box
+                   
+            ), # END left-hand column
+            
+            
+            # right-hand column ----
+            column(width = 6,
+                   
+                   # first fluidRow ----
+                   fluidRow(
+                     
+                     # data source box ----
+                     box(width = NULL,
+                         
+                         "data citation here"
+                         
+                     ) # END data source box
+                     
+                   ), # END first fluidRow
+                   
+                   # second fluidRow ----
+                   fluidRow(
+                     
+                     # disclaimer box ----
+                     box(width = NULL,
+                         
+                         "disclaimer here"
+                         
+                     ) # END disclaimer box
+                     
+                   ) # END second fluidRow
+                   
+            ) # END right-hand column
             
     ), # END welcome tabItem
     
     # dashboard tabItem ----
     tabItem(tabName = "dashboard",
             
-            "dashboard content here"
+            # input box ----
+            box(width = 4,
+                
+                title = tags$strong("Adjust lake parameter ranges: "),
+                
+                # sliderInputs ----
+                sliderInput(inputId = "elevation_slider_input", label = "Elevation (meters above SL):",
+                            min = min(lake_data$Elevation), max = max(lake_data$Elevation),
+                            value = c(min(lake_data$Elevation), max(lake_data$Elevation))),
+                
+                sliderInput(inputId = "depth_slider_input", label = "Average depth (meters):",
+                            min = min(lake_data$AvgDepth), max = max(lake_data$AvgDepth),
+                            value = c(min(lake_data$AvgDepth), max(lake_data$AvgDepth))),
+                
+                sliderInput(inputId = "temp_slider_input", label = "Average lake bed temperature (degrees C):",
+                            min = min(lake_data$AvgTemp), max = max(lake_data$AvgTemp),
+                            value = c(min(lake_data$AvgTemp), max(lake_data$AvgTemp)))
+                
+            ), # END input box # END input box
+            
+            # leaflet box ----
+            box(width = 8, 
+                
+                title = strong("Monitoring Lakes Within Fish Creek Watershed"),
+                
+                # leaflet output ----
+                leafletOutput(outputId = "lake_map_output") |> 
+                  withSpinner(type = 1, color = "#4287f5") # for loading spinner
+                
+                
+            ) # END leaflet box
             
     ) # END dashboard tabItem
     
